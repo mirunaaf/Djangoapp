e3cf15be7676d03522d0f95e2b55eafa392ee887
mirunaaf <filfan.miruna@gmsil.com>
2025-01-30 23:46:20 +0000

    add record

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Tour
 # Create your views here.
 
 def index(request):
-    return render(request, 'tours/index.html')
+    tours = Tour.objects.all()
+    context ={'tours':tours}
+    return render(request, 'tours/index.html', context)
