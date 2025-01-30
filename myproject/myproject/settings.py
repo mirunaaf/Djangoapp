6e5080d19ff27fee771ba55908cae8a47d18f14b
mirunaaf <filfan.miruna@gmsil.com>
2025-01-30 22:09:20 +0000

    created model tour

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'myapp.apps.MyappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,12 +75,26 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+"""
+DATABASES = {
+    'default': {
+       'ENGINE':'django.db.backends.mysql',
+       'NAME':'myapp',
+       'USER':'root',
+       'PASSWORD':'password',
+       'HOST':'localhost',
+       'PORT':'3306',
+    }
+}
+"""
 
 
 # Password validation
